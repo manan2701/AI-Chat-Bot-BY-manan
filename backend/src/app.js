5030ea9df0a59979b49d0300e67ae50d12dbf89b
manan2701 <mananborda3@gmail.com>
2025-10-17 18:55:30 +0530

    v0.01

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,36 @@
-const expess = require('express');
+const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const path = require('path');
+
+
+/* Routes */
 const authRoutes = require('./routes/auth.routes');
-const chatRoutes = require('./routes/chat.route');
-const app = expess();
+const chatRoutes = require("./routes/chat.route");
 
-app.use(expess.json());
+
+const app = express();
+
+
+
+/* using middlewares */
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
 
+
+
+/* Using Routes */
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
+
+
+app.get("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = app;
